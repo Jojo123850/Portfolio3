324b4e51efdccfa589ab640716b669565fbf1458
Jojo123850 <giovanieandrianirina@gmail.com>
2026-03-24 16:35:18 +0100

    fix: update2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Accueil from './pages/Home';
 import Projects from './pages/Projects';
-// import Contact from './pages/Contact';
-// import NotFound from './pages/NotFound';
 
 function App() {
 	return(
 	  <Routes>
         <Route path="/" element={<Accueil/>} />
         <Route path="/Projects" element={<Projects/>} />
-        {/* <Route path="/contact" element={<Contact/>} />
-        <Route path="*" element={<NotFound/>} /> */}
     </Routes>
     
 	);
